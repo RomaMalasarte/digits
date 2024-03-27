73201e88be2403b913fd25f865535cfd4142fde2
Roma Malasarte <romaamor@hawaii.edu>
2024-03-26 22:37:31 -1000

    list contacts page-2 mockup finished in 24 minutes.
    Av performance

---
+++ app/imports/ui/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+Contact.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default Contact;
