8624d894e4a97ba7e281b5534ad491a04640bcfe
Roma Malasarte <romaamor@hawaii.edu>
2024-03-28 22:15:25 -1000

    contacts collection-1 DNF in 42 minutes.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -16,3 +16,16 @@ if (Stuffs.collection.find().count() === 0) {
     Meteor.settings.defaultData.forEach(data => addData(data));
   }
 }
+
+const addContact = (contact) => {
+  console.log(`Adding: ${contact.lastName} (${contact.owner})`);
+  Contacts.collection.insert(contact);
+};
+
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default contacts.');
+    Meteor.settings.defaultContacts.forEach(contact => addContact(contact));
+  }
+}
+
